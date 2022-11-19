2cca55cc55ae52822f2638ee99f01cbe1ff8931a
Evgeniy <estrin.j@yandex.ru>
2022-11-19 09:36:54 +0300

    Zadacha3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Write numbers!");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        
+        if (num1 % 2 == 0)
+        {
+        Console.WriteLine("Четное"); 
+        }
+        else 
+        {
+        Console.WriteLine("Нечетное");
+        }
+        
+        

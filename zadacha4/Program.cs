b7df30cb836570a5341b7de0df9658723530e8cf
Evgeniy <estrin.j@yandex.ru>
2022-11-19 12:17:51 +0300

    zadacha4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Write numbers!");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int i=0;
+     while (i<num1)      
+        {i++;
+        if (i%2==0)
+        Console.WriteLine(i); 
+        }
+        

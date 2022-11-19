8734e72bc6a847139e3fbb70b733bedaeb86b7a7
Evgeniy <estrin.j@yandex.ru>
2022-11-19 09:27:41 +0300

    Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Write numbers!");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("num1 the biggest");
+        }
+        else if (num2 > num1)
+        {
+            Console.WriteLine("num2 the biggest!");
+        }
+        else
+        {
+            Console.WriteLine("Одинаковые");
+        }

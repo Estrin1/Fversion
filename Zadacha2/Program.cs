9525b11acf62cfa472d2a7844ac187005e2b6f7c
Evgeniy <estrin.j@yandex.ru>
2022-11-19 09:31:35 +0300

    Zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Write numbers!");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int num3 = Convert.ToInt32(Console.ReadLine());
+        
+        int max = num1;
+        if (max < num2)  max = num2;
+        if (max < num3)  max = num3;
+       
+         Console.WriteLine($"Самое большое {max}");
+        
